ea73224ced358f790ddf13e575071f95277b07fe
Vishnu Verma <vishnuverma7524@gmail.com>
2020-04-09 01:08:25 +0530

    Initial commit

---
+++ src/store.js
@@ -0,0 +1,53 @@
+const NEW_TODO_CHANGED = "NEW_TODO_CHANGED";
+const ADD_TODO = "ADD_TODO";
+
+const initialState = {
+  //creating a store
+
+  message: "Hello Coding Garden!!",
+  newTodo: "",
+  todos: [
+    {
+      title: "Learn React",
+      done: false
+    },
+    {
+      title: "Learn JSX",
+      done: false
+    }
+  ]
+};
+
+export const actions = {
+  newTodoChanged(newTodo) {
+    return {
+      type: NEW_TODO_CHANGED,
+      newTodo
+    };
+  },
+  addTodo(todo) {
+    return {
+      type: ADD_TODO,
+      todo
+    };
+  }
+};
+
+export function reducer(state = initialState, action) {
+  switch (action.type) {
+    case NEW_TODO_CHANGED: {
+      return {
+        ...state,
+        newTodo: action.newTodo
+      };
+    }
+    case ADD_TODO: {
+      return {
+        ...state,
+        todos: [...state.todos, action.todo]
+      };
+    }
+    default:
+      return state;
+  }
+}
